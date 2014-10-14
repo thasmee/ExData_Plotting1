752c375b82a0a717dcac89af4fa54cf4732fa9ca
thasmee <thasmee@dsv.su.se>
2014-10-15 00:33:53 +0200

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,6 +1,13 @@
 ##Plot 2
-#Plot using ggplot2
-library(lubridate)
-#library(ggplot2)
-source("Plot1.R")
+##Graph is included as a comment to the code
+
+source("Plot1.R")#reload the data table and the variables
+
+
+##Changing the date and time formats
+datetime<-paste(q$Date,q$Time)
+DateTime<-strptime(datetime, format = "%d/%m/%Y %H:%M:%S")
+Data<-cbind(q,DateTime)
+
 plot(DateTime, as.numeric(q$Global_active_power), ylab ="Global Active Power(Killowatt)",xlab="", type="l")
+
