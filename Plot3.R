b846fd5046112dee7c2e85660514a9cbf788dd3a
thasmee <thasmee@dsv.su.se>
2014-10-15 00:04:44 +0200

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,8 +1,8 @@
 ##Plot 3
 
-source("Plot1.R") # load all the required variables and data tables
+#source("Plot1.R") # load all the required variables and data tables
 plot(DateTime, as.numeric(q$Sub_metering_1), type="n", xlab=" ",ylab="Energy sub meetering") #set the canves and the axes
 lines(DateTime, as.numeric(q$Sub_metering_1))
 lines(DateTime, as.numeric(q$Sub_metering_2), col="red")
 lines(DateTime, as.numeric(q$Sub_metering_3), col="blue")
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(30, 1),col=c("black","blue","red"))#set the legend
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(1, 1,1),col=c("black","blue","red"))#set the legend
