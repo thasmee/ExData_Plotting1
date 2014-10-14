b813e244d1f2bf86b6a07663df398d249b489970
thasmee <thasmee@dsv.su.se>
2014-10-14 23:35:14 +0200

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,6 +1,19 @@
-##Plot 2
-#Plot using ggplot2
-library(lubridate)
-#library(ggplot2)
-source("Plot1.R")
-plot(DateTime, as.numeric(q$Global_active_power), ylab ="Global Active Power(Killowatt)",xlab="", type="l")
+##Plot 4
+#
+source("Plot1.R")#initialize the data table and variables
+par(mfrow=c(2,2))#2 x 2 grid 
+
+#first graph
+plot(DateTime, as.numeric(q$Global_active_power), ylab ="Global Active Power",xlab="", type="l")
+#Second graph
+plot(DateTime, as.numeric(q$Voltage), ylab ="Voltage",xlab="datetime", type="l")
+
+#canvas for the 3rd graph
+plot(DateTime, as.numeric(q$Sub_metering_1), type="n",xlab=" ",ylab="Global active power")
+lines(DateTime, as.numeric(q$Sub_metering_1))
+lines(DateTime, as.numeric(q$Sub_metering_2), col="red")
+lines(DateTime, as.numeric(q$Sub_metering_3), col="blue")
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(30, 1),col=c("black","blue","red"))
+
+#fourth graph
+plot(DateTime, as.numeric(q$Global_reactive_power), ylab ="Global_reactive_power",xlab="datetime", type="l")
